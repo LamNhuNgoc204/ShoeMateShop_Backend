64277b961eb4b066fee75e1ffcb524b05eb3dd47
Lam Nhu Ngoc <156599086+LamNhuNgoc204@users.noreply.github.com>
2024-09-28 18:52:05 +0700

    Lay thong tin nguoi dung

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,10 +1,11 @@
 var express = require("express");
 var router = express.Router();
 const userController = require("../controllers/usersController");
+const { validateRequest } = require("../middlewares/userMiddleware");
 
 // url: http://localhost:8080/users
 
 // Get user information
-router.get("/user-infor/:userId", userController.getUserInfo);
+router.get("/user-infor/:userId", validateRequest, userController.getUserInfo);
 
 module.exports = router;
