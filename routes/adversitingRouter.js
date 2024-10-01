32a61e275f55e9360d2748cab0da35780120d38b
duc1504 <ngocducofficial@gmail.com>
2024-10-01 22:07:23 +0700

    be14 api getall adversitiong and filter

--- routes/adversitingRouter.js
+++ routes/adversitingRouter.js
@@ -9,4 +9,7 @@ router.post("/create", protect, adminOrEmployee, adversitingController.createAd)
 // Cập nhật quảng cáo (Chỉ admin hoặc nhân viên)
 router.put("/update/:id", protect, adminOrEmployee, adversitingController.updateAd);
 
+// Lấy danh sách tất cả các quảng cáo và lọc
+router.get("/all", protect, adversitingController.getAllAds);
+
 module.exports = router;
