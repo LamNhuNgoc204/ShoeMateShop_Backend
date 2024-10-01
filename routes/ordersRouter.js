5988a7fdff780c97561cb0533bf86b7de9a6c0cb
nguyennambac <gokuvtru7@gmail.com>
2024-10-02 01:33:35 +0700

    Be-5.1/Api-tao-don-hang-moi

--- routes/ordersRouter.js
+++ routes/ordersRouter.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const orderController = require("../controllers/ordersController");
 
+//  http://localhost:3000/orders
+
+//Create a new order
+router.post("/create-new-order", orderController.createNewOrder);
 
 module.exports = router;
