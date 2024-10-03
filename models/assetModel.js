2cb280c32ebe2abd8e902fea03920a48d75473de
Lam Nhu Ngoc <156599086+LamNhuNgoc204@users.noreply.github.com>
2024-10-03 10:27:29 +0700

    delete assetmodel

--- models/assetModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const assetSchema = new mongoose.Schema({
-  assetUrl: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.models.assets || mongoose.model("asset", assetSchema);
