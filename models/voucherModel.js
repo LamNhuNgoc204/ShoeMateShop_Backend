cdfbd868ab169438c83f9448af3ed98d745ab41d
Lam Nhu Ngoc <156599086+LamNhuNgoc204@users.noreply.github.com>
2024-10-25 00:03:56 +0700

    Thêm property vào voucher

--- models/voucherModel.js
+++ models/voucherModel.js
@@ -5,15 +5,17 @@ const voucherSchema = new mongoose.Schema({
   voucher_name: { type: String, required: true },
   quantity: { type: Number, required: true, default: 1 },
   voucher_image: { type: String },
-  voucher_code: { type: String, required: true, unique: true }, 
+  voucher_code: { type: String, required: true, unique: true },
   expiry_date: { type: Date, required: true },
+  start_date: { type: Date, required: true },
   min_order_value: { type: Number, required: true },
   max_discount_value: { type: Number, required: true },
   usage_scope: { type: String },
   status: { type: String, enum: ["active", "inactive"], default: "active" },
   isInMiniGame: { type: Boolean, default: false },
-  usedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  usedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "user" }],
   createdAt: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.models.vouchers || mongoose.model("voucher", voucherSchema);
+module.exports =
+  mongoose.models.vouchers || mongoose.model("voucher", voucherSchema);
