1e75fd7e03a27bbc5f71539f86d76488a799676a
Lam Nhu Ngoc <156599086+LamNhuNgoc204@users.noreply.github.com>
2024-10-27 21:58:41 +0700

    Update model wallet

--- models/walletModel.js
+++ models/walletModel.js
@@ -8,6 +8,8 @@ const walletSchema = new mongoose.Schema({
   money: { type: Number, default: 0 },
   point: { type: Number, default: 0 },
   pin: { type: String, required: true },
+  create_at: { type: Date, default: Date.now },
+  update_at: { type: Date, default: Date.now },
 });
 
 module.exports =
