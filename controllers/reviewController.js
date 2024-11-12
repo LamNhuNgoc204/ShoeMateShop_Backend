8b1f554521fc917dd2961587722f86b1b6cd52f6
Lam Nhu Ngoc <156599086+LamNhuNgoc204@users.noreply.github.com>
2024-11-12 19:09:38 +0700

    Xóa code thừa

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -138,49 +138,6 @@ exports.createMultipleReviews = async (req, res) => {
   }
 };
 
-exports.createReview = async (req, res) => {
-  try {
-    const { orderDetail_id, product_id, rating, comment, images, video } =
-      req.body;
-
-    const reviewer_id = req.user._id;
-
-    const orderdetail = await OrderDetail.findById(orderDetail_id);
-    if (!orderdetail) {
-      return res
-        .status(400)
-        .json({ status: false, message: "Order detail not found" });
-    }
-
-    const product = await Product.findById(product_id);
-    if (!product) {
-      return res
-        .status(400)
-        .json({ status: false, message: "Product detail not found" });
-    }
-
-    const newReview = new Review({
-      orderDetail_id,
-      product_id,
-      reviewer_id,
-      rating,
-      comment,
-      images,
-      video,
-    });
-    await newReview.save();
-
-    return res.status(200).json({
-      status: true,
-      message: "Review created successfully.",
-      data: newReview,
-    });
-  } catch (error) {
-    console.log("Error: ", error);
-    return res.status(500).json({ status: false, message: "Server error" });
-  }
-};
-
 // for admin
 exports.updateReviewStatus = async (req, res) => {
   try {
