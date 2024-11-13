258e89351aa7a4bceb1a0184ef8f73f0f0cd9e74
Lam Nhu Ngoc <156599086+LamNhuNgoc204@users.noreply.github.com>
2024-11-13 22:19:30 +0700

    Fix logic get user review

--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const reviewController = require("../controllers/reviewController");
 const {
-  createReview,
   checkUpdateReview,
   checkReviewById,
   checkProductById,
